b47048bcba34a471a78100c482bbd3edbb31759c
Do Viet Dung <dovietdungsl@gmail.com>
2025-12-01 16:13:31 +0700

    [MODIFY] modify sender.py to test ecb

--- src_py/sender.py
+++ src_py/sender.py
@@ -1,17 +1,35 @@
-from src_py.aes_ops import encrypt_cbc
+# from src_py.aes_ops import encrypt_cbc
+
+# def main():
+#     key = b"thisisakey123456"
+
+#     message = input("Enter plaintext: ")
+#     plaintext = message.encode("utf-8")
+
+#     # iv = input("Enter iv:")
+#     ciphertext, iv = encrypt_cbc(plaintext, key, iv=None)
+
+#     print("Private key:", key.hex())
+#     print("IV  (public):   ", iv.hex())
+#     print("Ciphertext (public)", ciphertext.hex())
+
+# if __name__ == "__main__":
+#     main()
+from src_py.aes_ops.aes_ecb import encrypt_ecb
 
 def main():
+    # 16-byte key (giống AES-128)
     key = b"thisisakey123456"
 
+    # Nhập plaintext từ người dùng
     message = input("Enter plaintext: ")
     plaintext = message.encode("utf-8")
 
-    # iv = input("Enter iv:")
-    ciphertext, iv = encrypt_cbc(plaintext, key, iv=None)
+    # ECB không dùng IV, nên chỉ cần ciphertext
+    ciphertext = encrypt_ecb(key, plaintext)
 
-    print("Private key:", key.hex())
-    print("IV  (public):   ", iv.hex())
-    print("Ciphertext (public)", ciphertext.hex())
+    print("Private key:            ", key.hex())
+    print("Ciphertext (public):    ", ciphertext.hex())
 
 if __name__ == "__main__":
     main()
