23f8d20c42aeca81c9496b96ae63d3c64d6c62f6
Do Viet Dung <dovietdungsl@gmail.com>
2025-12-01 16:28:45 +0700

    [MODIFY] modify receiver.py to test ecb

--- src_py/receiver.py
+++ src_py/receiver.py
@@ -1,20 +1,38 @@
-from src_py.aes_ops import decrypt_cbc
+# from src_py.aes_ops import decrypt_cbc
+
+# def main():
+#     key_hex = input("Enter key (hex): ").strip()
+#     iv_hex = input("Enter IV (hex): ").strip()
+#     ct_hex = input("Enter ciphertext (hex): ").strip()
+
+#     key = bytes.fromhex(key_hex)
+#     iv = bytes.fromhex(iv_hex)
+#     ciphertext = bytes.fromhex(ct_hex)
+
+#     try:
+#         plaintext = decrypt_cbc(ciphertext, key, iv)
+#         print("\nDecrypted text:")
+#         print(plaintext.decode("utf-8"))
+#     except Exception as e:
+#         print(f"\nDecryption failed (wrong key or error).\n{e}")
+
+# if __name__ == "__main__":
+#     main()
+from src_py.aes_ops.aes_ecb import decrypt_ecb
 
 def main():
     key_hex = input("Enter key (hex): ").strip()
-    iv_hex = input("Enter IV (hex): ").strip()
     ct_hex = input("Enter ciphertext (hex): ").strip()
 
     key = bytes.fromhex(key_hex)
-    iv = bytes.fromhex(iv_hex)
     ciphertext = bytes.fromhex(ct_hex)
 
     try:
-        plaintext = decrypt_cbc(ciphertext, key, iv)
+        plaintext = decrypt_ecb(key, ciphertext)
         print("\nDecrypted text:")
         print(plaintext.decode("utf-8"))
     except Exception as e:
-        print(f"\nDecryption failed (wrong key or error).\n{e}")
+        print(f"\nDecryption failed (wrong key or error):\n{e}")
 
 if __name__ == "__main__":
     main()
